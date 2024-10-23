32cedf1b5637c80d5b074022c447405d583ca9b8
Savinoff94 <andreisavinovwork@gmail.com>
2024-10-23 09:07:39 +0300

    stylelint, plugin for i18next(eslint) marks not translated srtings

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,7 +3,7 @@ import { useTheme, Theme } from 'app/providers/ThemeProviders';
 import LightIcon from 'shared/assets/icons/theme-light 1.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark 1.svg';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import cls from './ThemeSwitcher.module.scss';
+// import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
     className ?: string
@@ -14,7 +14,7 @@ export const ThemeSwitcher = ({ className } : ThemeSwitcherProps) => {
     return (
         <Button
             theme={ThemeButton.CLEAR}
-            className={classNames(cls.ThemeSwitcher, {}, [])}
+            className={classNames('', {}, [className])}
             onClick={toggleTheme}
         >
             {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
